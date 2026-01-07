c2298a08bf992140096305c68642fa5b1767d4c2
igorfazlyev <igorfazlyev@gmail.com>
2026-01-07 14:59:32 +0000

    some minor inconsequential changes

--- go-movies-back-end/cmd/api/middleware.go
+++ go-movies-back-end/cmd/api/middleware.go
@@ -6,7 +6,7 @@ func (app *Application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//w.Header().Set("Access-Control-Allow-Origin", "http://*")
 		//https://bookish-spork-974xg5r4jv5h749v-5173.app.github.dev/
-		w.Header().Set("Access-Control-Allow-Origin", "https://bookish-spork-974xg5r4jv5h749v-5173.app.github.dev")
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Credentials", "ture")
 			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
